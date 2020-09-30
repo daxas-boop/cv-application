9bdba23e23c346ac24b524d4c694d872ebf03c19
daxas-boop <daxas.swd@hotmail.com>
2020-09-29 23:31:27 -0300

    Sektch of page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react"
+import tw, { styled, css } from 'twin.macro';
+import BackgroundImage from './images/test.jpg'
+import Cv from './components/cv'
+
+const Container = styled.div([
+  css `
+    background-image: url(${BackgroundImage});
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center left;
+    min-height: 100vh;
+  `,
+  tw `flex items-center`
+])
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Container>
+      <Cv></Cv>
+    </Container>
   );
 }
 
