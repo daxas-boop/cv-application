21e5039b9a156a5e4cd76d6836d64b832bbfdacf
daxas-boop <daxas.swd@hotmail.com>
2020-10-02 00:28:33 -0300

    Adds queries for mobile. Changes layout sidenav goes to the bottom of the page on mobile

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import tw, { styled, css } from 'twin.macro';
 import BackgroundImage from './images/test.jpg';
 import Header from './components/Header';
 import Body from './components/body/Body';
-import SideNav from "./components/SideNav";
+import SideNav from "./components/sidenav/SideNav";
 
 const Container = styled.div([
   css `
+    background-color:#eee;
     background-image: url(${BackgroundImage});
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-size: cover;
     background-position: center left;
     min-height: 100vh;
+    @media (max-width: 640px) {
+      background-image:none;
+    }
   `,
   tw `flex items-center`
 ])
@@ -20,9 +24,18 @@ const Container = styled.div([
 const Wrapper = styled.div([
   css `
     min-height: 98vh;
+    max-width: 100vw;
     box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+    @media (max-width: 640px) {
+      min-height: 100vh;
+    }
   `,
-  tw `container md:w-3/4 lg:w-3/4 xl:w-1/2 mx-auto rounded grid grid-rows-5 grid-cols-3 relative`
+  tw `container mx-auto rounded grid relative
+      grid-rows-3 grid-cols-3
+      sm:(grid-rows-5 grid-cols-3)
+      md:w-3/4 
+      lg:w-3/4 
+      xl:w-1/2`
 ])
 
 function App() {
--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -7,7 +7,10 @@ const Container = styled.div([
     css ` 
         font-family: 'Roboto', sans-serif;
     `,
-    tw `container bg-gray-100 col-start-2 col-end-4 row-start-2 row-end-6 rounded-r-sm rounded-t-none`
+    tw `container bg-gray-100 rounded-r-sm rounded-t-none
+        col-start-1 col-end-4 row-start-2 row-end-4
+        sm:(col-start-2 col-end-4 row-start-2 row-end-6)
+    `
 ])
 
 class Body extends Component{
