aed8bac3882f255384cfff160745613a682804a2
daxas-boop <daxas.swd@hotmail.com>
2020-10-03 23:41:15 -0300

    Updates

---
+++ precache-manifest.7070f30845ce79bb02692953e3430f58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844ba0e6d49b8c2bbdc994af6284f37a",
+    "url": "/cv-application/index.html"
+  },
+  {
+    "revision": "c49d65432c9a97d64e98",
+    "url": "/cv-application/static/css/2.4c4eb0a5.chunk.css"
+  },
+  {
+    "revision": "c49d65432c9a97d64e98",
+    "url": "/cv-application/static/js/2.b4c3d3d5.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/cv-application/static/js/2.b4c3d3d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b697838f0f37335ee28e",
+    "url": "/cv-application/static/js/main.b9568137.chunk.js"
+  },
+  {
+    "revision": "4a69fa3c6cf210a1d2d8",
+    "url": "/cv-application/static/js/runtime-main.26e5b48e.js"
+  },
+  {
+    "revision": "9ae837bda0d5f26b1198cd3c4a9f638a",
+    "url": "/cv-application/static/media/test.9ae837bd.jpg"
+  }
+]);
